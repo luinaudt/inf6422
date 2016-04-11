6c4f2fedee39314169aebee604354e2763d4c15e
PaulTPT <paul.berthier@outlook.com>
2016-04-11 19:10:28 -0400

    Travail sur le powerpoint et corrections de la simu Matlab

--- lab3/Paul/Matlab/modelisation.m
+++ lab3/Paul/Matlab/modelisation.m
@@ -6,8 +6,8 @@ lambda2=0.03;
 k1=1;
 k2=1;
 Hun=log2(n);
-[T, H] = meshgrid(0:1:100, 0:Hun/100:Hun);
+[T, H] = meshgrid(1:1:100, 0:Hun/100:Hun);
 Nun=(1-exp(-lambda1*T)).*(m-(m-1)*H/Hun);
-C=(1+(n-1)*(2*H/Hun).^2)+ 4*n*exp(-lambda2*T)
+C=4*n*(H/Hun).^4+ m*T.^-1
 P=Nun*k1+C*k2;
 surf(T,H,P)
